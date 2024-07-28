291826b161c0426d2ad6547adafd024b6000638d
xSolomon <125121207+xSolomon@users.noreply.github.com>
2024-07-28 13:25:56 +0700

    add lesson 3 solutions

---
+++ sql3/1.sql
@@ -0,0 +1,2 @@
+SELECT * FROM Orders
+WHERE (Freight BETWEEN 100 AND 200) and (ShipCountry in ('USA', 'France')
---
+++ sql3/3.sql
@@ -0,0 +1,2 @@
+SELECT * FROM EmployeeTerritories
+WHERE (TerritoryID BETWEEN 6897 and 31000)
