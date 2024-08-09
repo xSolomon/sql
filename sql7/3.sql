6dd73791b61aa14bc7a2d99e8e915de7ce398c7c
xSolomon <125121207+xSolomon@users.noreply.github.com>
2024-08-09 15:05:46 +0700

    Add files via upload

--- sql7/3.sql
+++ sql7/3.sql
@@ -1,5 +1,5 @@
 SELECT Products.ProductName, Categories.CategoryName, [Order Details].UnitPrice
 FROM Products, [Order Details], Categories
 WHERE [Order Details].ProductID = Products.ProductID
-	AND Products.CategoryID = Categories.CategoryID
 	AND [Order Details].UnitPrice < 20
+	AND Products.CategoryID = Categories.CategoryID
