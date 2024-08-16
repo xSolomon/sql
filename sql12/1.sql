68b4b37e8beaf8660df52778c2e1b4267d2b8a9a
xSolomon <125121207+xSolomon@users.noreply.github.com>
2024-08-17 00:23:11 +0700

    Add files via upload

---
+++ sql12/1.sql
@@ -0,0 +1,3 @@
+UPDATE [Order Details]
+SET Discount = '0.20'
+WHERE Quantity > 50;
---
+++ sql12/2.sql
@@ -0,0 +1,3 @@
+UPDATE Contacts
+SET City = 'Piter', Country = 'Russia'
+WHERE City = 'Berlin' AND Country = 'Germany'
